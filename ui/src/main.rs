a0651ac54ee4c4284e39867dba539c87069d4831
Shem (RetroBreeze) <retrobreezeyt@gmail.com>
2025-07-31 14:59:54 +0100

    Fix skeleton

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,11 +1,13 @@
-use gdk4;
-use gtk::glib;
-use gtk::glib::clone;
-use gtk::{prelude::*, Application, ApplicationWindow, Orientation};
 use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::{Align, Application, ApplicationWindow, Orientation};
+use gtk::gdk;
+use gtk::glib;
+use std::rc::Rc;
 use gtk4_layer_shell::{self as layer_shell, LayerShell};
 
 fn build_ui(app: &Application) {
+    // Main window setup
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Loki Control Center")
@@ -18,34 +20,37 @@ fn build_ui(app: &Application) {
     window.set_anchor(layer_shell::Edge::Right, true);
     window.set_exclusive_zone(0);
 
-    if let Some(display) = gdk4::Display::default() {
-        let monitors = display.monitors();
-        if let Some(obj) = monitors.item(0) {
-            if let Ok(monitor) = obj.downcast::<gdk4::Monitor>() {
-                let geo = monitor.geometry();
-                let width = ((geo.width() as f32) * 0.3) as i32;
-                window.set_default_size(width, geo.height());
+    // Size to 30% of screen width
+    if let Some(display) = gdk::Display::default() {
+        if let Some(obj) = display.monitors().item(0) {
+            if let Some(mon) = obj.downcast_ref::<gdk::Monitor>() {
+                let geo = mon.geometry();
+                let w = ((geo.width() as f32) * 0.3) as i32;
+                window.set_default_size(w, geo.height());
             }
         }
     }
 
+    // Dark theme
     if let Some(settings) = gtk::Settings::default() {
         settings.set_gtk_application_prefer_dark_theme(true);
     }
 
+    // Scrollable container
     let scrolled = gtk::ScrolledWindow::builder()
         .vexpand(true)
         .hexpand(true)
         .hscrollbar_policy(gtk::PolicyType::Never)
         .build();
-
     let vbox = gtk::Box::new(Orientation::Vertical, 12);
     vbox.set_margin_top(16);
     vbox.set_margin_bottom(16);
     vbox.set_margin_start(16);
     vbox.set_margin_end(16);
 
+    // Row 1: Connectivity buttons centered
     let row1 = gtk::Box::new(Orientation::Horizontal, 8);
+    row1.set_halign(Align::Center);
     for label in ["Wi-Fi", "Bluetooth", "Airplane"] {
         let btn = gtk::Button::with_label(label);
         btn.add_css_class("circular");
@@ -53,26 +58,38 @@ fn build_ui(app: &Application) {
     }
     vbox.append(&row1);
 
+    // Row 2: Brightness slider + label
+    let row2 = gtk::Box::new(Orientation::Horizontal, 8);
+    row2.set_valign(Align::Center);
+    let bright_label = gtk::Label::new(Some("Brightness:"));
     let brightness = gtk::Scale::with_range(Orientation::Horizontal, 0.0, 100.0, 1.0);
     brightness.set_value(50.0);
-    vbox.append(&brightness);
+    row2.append(&bright_label);
+    row2.append(&brightness);
+    vbox.append(&row2);
 
+    // Row 3: Volume slider + label + mute
     let row3 = gtk::Box::new(Orientation::Horizontal, 8);
+    row3.set_valign(Align::Center);
+    let volume_label = gtk::Label::new(Some("Volume:"));
     let volume = gtk::Scale::with_range(Orientation::Horizontal, 0.0, 100.0, 1.0);
     volume.set_hexpand(true);
     let mute = gtk::ToggleButton::with_label("Mute");
+    row3.append(&volume_label);
     row3.append(&volume);
     row3.append(&mute);
     vbox.append(&row3);
 
     vbox.append(&gtk::Separator::new(Orientation::Horizontal));
 
+    // Row 4: Resolution dropdown
     let row4 = gtk::Box::new(Orientation::Horizontal, 8);
     let res_combo = gtk::DropDown::from_strings(&["1080p", "720p"]);
     row4.append(&gtk::Label::new(Some("Resolution:")));
     row4.append(&res_combo);
     vbox.append(&row4);
 
+    // Row 5: Refresh rate buttons
     let row5 = gtk::Box::new(Orientation::Horizontal, 8);
     for hz in ["40Hz", "50Hz", "60Hz"] {
         row5.append(&gtk::Button::with_label(hz));
@@ -81,10 +98,27 @@ fn build_ui(app: &Application) {
 
     vbox.append(&gtk::Separator::new(Orientation::Horizontal));
 
+    // Row 6: TDP slider + snap & label
+    let row6 = gtk::Box::new(Orientation::Horizontal, 8);
+    row6.set_valign(Align::Center);
+    let tdp_label = gtk::Label::new(Some("TDP (W):"));
     let tdp = gtk::Scale::with_range(Orientation::Horizontal, 5.0, 28.0, 1.0);
     tdp.set_value(15.0);
-    vbox.append(&tdp);
+    let tdp_value = gtk::Label::new(Some(&format!("{} W", tdp.value() as i32)));
+    {
+        let tdp_value_cl = tdp_value.clone();
+        tdp.connect_value_changed(move |s| {
+            let v = s.value().round();
+            s.set_value(v);
+            tdp_value_cl.set_text(&format!("{} W", v as i32));
+        });
+    }
+    row6.append(&tdp_label);
+    row6.append(&tdp);
+    row6.append(&tdp_value);
+    vbox.append(&row6);
 
+    // Row 7: Fan profile radio‐style
     let row7 = gtk::Box::new(Orientation::Horizontal, 8);
     let silent = gtk::CheckButton::with_label("Silent");
     let auto = gtk::CheckButton::with_label("Auto");
@@ -96,98 +130,144 @@ fn build_ui(app: &Application) {
     row7.append(&manual);
     vbox.append(&row7);
 
+    // Row 8: Manual fan speed
     let manual_speed = gtk::Scale::with_range(Orientation::Horizontal, 0.0, 100.0, 1.0);
-    manual_speed.set_value(50.0);
     manual_speed.set_visible(false);
-    manual.connect_toggled(clone!(@weak manual_speed => move |btn| {
-        manual_speed.set_visible(btn.is_active());
-    }));
+    {
+        let ms = manual_speed.clone();
+        manual.connect_toggled(move |btn| ms.set_visible(btn.is_active()));
+    }
     vbox.append(&manual_speed);
 
     vbox.append(&gtk::Separator::new(Orientation::Horizontal));
 
     // RGB Lighting section
     let rgb_section = gtk::Box::new(Orientation::Vertical, 8);
-
     let rgb_label = gtk::Label::new(Some("RGB Lighting"));
     rgb_label.add_css_class("heading");
     rgb_section.append(&rgb_label);
     rgb_section.append(&gtk::Separator::new(Orientation::Horizontal));
 
+    // Color picker + preview
     let color_row = gtk::Box::new(Orientation::Horizontal, 8);
     let color_dialog = gtk::ColorDialog::new();
-    let color_button = gtk::ColorDialogButton::new(Some(&color_dialog));
+    let color_button = gtk::ColorDialogButton::new(Some(color_dialog));
     color_button.add_css_class("circular");
     let preview = gtk::Box::new(Orientation::Vertical, 0);
     preview.set_size_request(24, 24);
+    preview.add_css_class("rgb-preview");
     color_row.append(&color_button);
     color_row.append(&preview);
     rgb_section.append(&color_row);
 
+    // Advanced sliders toggle
     let advanced_switch = gtk::Switch::new();
     let advanced_row = gtk::Box::new(Orientation::Horizontal, 8);
     advanced_row.append(&gtk::Label::new(Some("Advanced sliders")));
     advanced_row.append(&advanced_switch);
     rgb_section.append(&advanced_row);
 
+    // R/G/B sliders
     let sliders = gtk::Box::new(Orientation::Vertical, 4);
     let red = gtk::Scale::with_range(Orientation::Horizontal, 0.0, 255.0, 1.0);
     let green = gtk::Scale::with_range(Orientation::Horizontal, 0.0, 255.0, 1.0);
     let blue = gtk::Scale::with_range(Orientation::Horizontal, 0.0, 255.0, 1.0);
-    for (label, scale) in [("Red:", &red), ("Green:", &green), ("Blue:", &blue)] {
+    for (lbl, scale) in [("Red:", &red), ("Green:", &green), ("Blue:", &blue)] {
         let row = gtk::Box::new(Orientation::Horizontal, 4);
-        row.append(&gtk::Label::new(Some(label)));
+        row.append(&gtk::Label::new(Some(lbl)));
         scale.set_hexpand(true);
         row.append(scale);
         sliders.append(&row);
     }
     sliders.set_visible(false);
+    {
+        let sl = sliders.clone();
+        advanced_switch.connect_state_set(move |_, on| {
+            sl.set_visible(on);
+            gtk::glib::Propagation::Proceed
+        });
+    }
     rgb_section.append(&sliders);
 
-    advanced_switch.connect_state_set(
-        clone!(@weak sliders => @default-return glib::Propagation::Proceed, move |_, state| {
-            sliders.set_visible(state);
-            glib::Propagation::Proceed
-        }),
+    // CSS provider for preview
+    let css_provider = gtk::CssProvider::new();
+    gtk::style_context_add_provider_for_display(
+        &gdk::Display::default().unwrap(),
+        &css_provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
     );
 
-    let brightness = gtk::Scale::with_range(Orientation::Horizontal, 0.0, 100.0, 1.0);
-    rgb_section.append(&brightness);
-
-    let effect_label = gtk::Label::new(Some("Effect: Static"));
-    effect_label.set_sensitive(false);
-    rgb_section.append(&effect_label);
-
-    // update preview color when values change
-    let css_provider = gtk::CssProvider::new();
-    preview
-        .style_context()
-        .add_provider(&css_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
-    let update_preview = move |color: gdk4::RGBA| {
-        let css = format!("background-color:{};", color.to_string());
-        css_provider.load_from_data(&css);
+    // Wrap our preview‐updater in an Rc so we can clone it into multiple handlers:
+    let update_preview = {
+        let prov = css_provider.clone();
+        Rc::new(move |color: gtk::gdk::RGBA| {
+            let css = format!(".rgb-preview {{ background-color: {}; }}", color.to_string());
+            prov.load_from_data(&css);
+        })
     };
-    update_preview(color_button.rgba());
-    color_button.connect_rgba_notify(clone!(@strong update_preview => move |btn| {
-        update_preview(btn.rgba());
-    }));
-    for scale in [&red, &green, &blue] {
-        scale.connect_value_changed(
-            clone!(@weak color_button, @strong update_preview => move |_| {
-                let mut color = color_button.rgba();
-                color.red = red.value() as f32 / 255.0;
-                color.green = green.value() as f32 / 255.0;
-                color.blue = blue.value() as f32 / 255.0;
-                color_button.set_rgba(&color);
-            }),
-        );
+
+    // Initialize preview to current picker color
+    (update_preview)(color_button.rgba());
+
+    // Hook up color dialog changes
+    {
+        let upd = update_preview.clone();
+        color_button.connect_rgba_notify(move |btn| {
+            upd(btn.rgba());
+        });
     }
 
-    rgb_section.set_spacing(8);
-    vbox.append(&rgb_section);
+    // Three independent R/G/B handlers:
+    {
+        let r = red.clone();
+        let g = green.clone();
+        let b = blue.clone();
+        let cb = color_button.clone();
+        let upd = update_preview.clone();
+        red.connect_value_changed(move |_| {
+            let mut c = cb.rgba();
+            c.set_red(r.value() as f32 / 255.0);
+            c.set_green(g.value() as f32 / 255.0);
+            c.set_blue(b.value() as f32 / 255.0);
+            cb.set_rgba(&c);
+            upd(c);
+        });
+    }
+    {
+        let r = red.clone();
+        let g = green.clone();
+        let b = blue.clone();
+        let cb = color_button.clone();
+        let upd = update_preview.clone();
+        green.connect_value_changed(move |_| {
+            let mut c = cb.rgba();
+            c.set_red(r.value() as f32 / 255.0);
+            c.set_green(g.value() as f32 / 255.0);
+            c.set_blue(b.value() as f32 / 255.0);
+            cb.set_rgba(&c);
+            upd(c);
+        });
+    }
+    {
+        let r = red.clone();
+        let g = green.clone();
+        let b = blue.clone();
+        let cb = color_button.clone();
+        let upd = update_preview.clone();
+        blue.connect_value_changed(move |_| {
+            let mut c = cb.rgba();
+            c.set_red(r.value() as f32 / 255.0);
+            c.set_green(g.value() as f32 / 255.0);
+            c.set_blue(b.value() as f32 / 255.0);
+            cb.set_rgba(&c);
+            upd(c);
+        });
+    }
 
+    vbox.append(&rgb_section);
     vbox.append(&gtk::Separator::new(Orientation::Horizontal));
 
+    // Row 10: Vibration toggle
     let row10 = gtk::Box::new(Orientation::Horizontal, 8);
     let vibration = gtk::CheckButton::with_label("Vibration");
     row10.append(&vibration);
@@ -196,8 +276,7 @@ fn build_ui(app: &Application) {
 
     scrolled.set_child(Some(&vbox));
     window.set_child(Some(&scrolled));
-
-    window.show();
+    window.present();
 }
 
 fn main() {
